1bdbcfbddefca347178b1fa4b4f6fdeef7645901
elisabeth <elisabeth-kluth@t-online.de>
2021-08-28 22:53:46 +0200

    WIP basic pong application

---
+++ FullScreenTriangle.h
@@ -0,0 +1,22 @@
+//
+// Created by janos on 7/4/20.
+//
+
+#pragma once
+
+#include <Magnum/GL/AbstractShaderProgram.h>
+#include <Magnum/Shaders/Generic.h>
+#include <Magnum/GL/GL.h>
+
+
+class FullScreenTriangle : public Magnum::GL::AbstractShaderProgram {
+public:
+    using Position = Magnum::Shaders::Generic3D::Position;
+
+    explicit FullScreenTriangle(Magnum::NoCreateT) : Magnum::GL::AbstractShaderProgram{Magnum::NoCreate} {};
+
+    explicit FullScreenTriangle();
+
+    FullScreenTriangle& bindTexture(Magnum::GL::Texture2D& texture);
+};
+
