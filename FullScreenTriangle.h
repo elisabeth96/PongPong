0a93dc9b8c9eeaa513bed3aec58097c14bb10765
Janos Meny <janos.meny@googlemail.com>
2021-08-29 13:17:42 +0200

    Install sdl2 on macos and fix some formatting

--- FullScreenTriangle.h
+++ FullScreenTriangle.h
@@ -1,22 +1,18 @@
-//
-// Created by janos on 7/4/20.
-//
-
 #pragma once
 
 #include <Magnum/GL/AbstractShaderProgram.h>
 #include <Magnum/Shaders/Generic.h>
 #include <Magnum/GL/GL.h>
 
-
-class FullScreenTriangle : public Magnum::GL::AbstractShaderProgram {
+class FullScreenTriangle : public Magnum::GL::AbstractShaderProgram
+{
 public:
     using Position = Magnum::Shaders::Generic3D::Position;
 
-    explicit FullScreenTriangle(Magnum::NoCreateT) : Magnum::GL::AbstractShaderProgram{Magnum::NoCreate} {};
+    explicit FullScreenTriangle(Magnum::NoCreateT)
+        : Magnum::GL::AbstractShaderProgram{Magnum::NoCreate} {};
 
     explicit FullScreenTriangle();
 
     FullScreenTriangle& bindTexture(Magnum::GL::Texture2D& texture);
 };
-
