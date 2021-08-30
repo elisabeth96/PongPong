4bb8edd0d2e1f45a4556f12bed89766dbdec7796
elisabeth <elisabeth-kluth@t-online.de>
2021-08-30 21:56:05 +0200

    improve paddle-ball interaction and add count

--- PongPongApplication.cpp
+++ PongPongApplication.cpp
@@ -11,19 +11,68 @@
 #include <Magnum/GL/Mesh.h>
 
 #include <vector>
+#include <assert.h>
 
 #include "FullScreenTriangle.h"
 #include <iostream>
 
 using namespace Magnum;
+
+using namespace Math::Literals;
+
 PongPongApplication::PongPongApplication(const Arguments& arguments)
-    : Platform::Application{arguments /*, Configuration().setSize({1024, 1024})*/}
+    : Platform::Application{arguments, Configuration()
+                                           .setSize({1024, 512})
+                                           .setTitle("PongPong")
+                                           .setWindowFlags(Configuration::WindowFlag::Resizable)}
+    , m_cache{Vector2i{2048}, Vector2i{512}, 22}
 {
     m_y_left      = windowSize().y() / 2;
     m_y_right     = windowSize().y() / 2;
     m_width_half  = windowSize().x() / 80;
     m_height_half = windowSize().y() / 10;
-    m_ball        = {windowSize() / 2, {1, 5}, windowSize().x() / 50};
+    m_ball        = {windowSize() / 2, {1, 0}, windowSize().x() / 50, 1.};
+    m_base_speed  = windowSize().x() / 140;
+
+    m_paddle_speed = windowSize().x() / 70;
+
+    // Allocate storage for pixel colours
+    m_colours.resize(windowSize().product());
+    //Give colours to image and get matrix
+    m_image_view = MutableImageView2D(PixelFormat::RGB32F, windowSize(), {m_colours.data(), m_colours.size()});
+    auto pixel = m_image_view.pixels<Color3>();
+    for (auto x = 0; x < m_image_view.size().x(); x++) {
+        for (auto y = 0; y < m_image_view.size().y(); y++) {
+            pixel[y][x] = {0, 0.3, 0.8};
+        }
+    }
+
+    /* Load a TrueTypeFont plugin and open the font */
+    Utility::Resource rs("pp-data");
+    m_font = m_manager.loadAndInstantiate("FreeTypeFont");
+    if(!m_font || !m_font->openData(rs.getRaw("SourceSansPro-Regular.ttf"), 180.0f))
+        Fatal{} << "Cannot open font file";
+
+    /* Glyphs we need to render everything */
+    m_font->fillGlyphCache(m_cache, "0123456789: ");
+
+    /* Dynamically updated text that shows rotation/zoom of the other. Size in
+       points that stays the same if you resize the window. Aligned so top
+       right of the bounding box is at mesh origin, and then transformed so the
+       origin is at the top right corner of the window. */
+    m_dynamicText.reset(new Text::Renderer2D(*m_font, m_cache, 32.0f, Text::Alignment::TopRight));
+    m_dynamicText->reserve(40, GL::BufferUsage::DynamicDraw, GL::BufferUsage::StaticDraw);
+    m_transformationProjectionDynamicText =
+        Matrix3::projection(Vector2{windowSize()})*
+        Matrix3::translation(Vector2{windowSize()}*0.5f);
+
+    /* Set up premultiplied alpha blending to avoid overlapping text characters
+       to cut into each other */
+    GL::Renderer::enable(GL::Renderer::Feature::Blending);
+    GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::One, GL::Renderer::BlendFunction::OneMinusSourceAlpha);
+    GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add, GL::Renderer::BlendEquation::Add);
+
+    updateText();
 }
 
 void PongPongApplication::keyPressEvent(KeyEvent& event)
@@ -41,6 +90,10 @@ void PongPongApplication::keyPressEvent(KeyEvent& event)
         m_left_player_state = PlayerState::Up;
     }
     redraw();
+}
+
+void PongPongApplication::updateText() {
+    m_dynamicText->render(Utility::formatString("{}:{}", m_left_score, m_right_score));
 }
 
 void PongPongApplication::keyReleaseEvent(KeyEvent& event)
@@ -89,7 +142,7 @@ void PongPongApplication::drawBall(const MutableImageView2D& image_view, const C
 
 void PongPongApplication::moveBall()
 {
-    m_ball.position += Vector2i(m_ball.direction);
+    m_ball.position += Vector2i(m_ball.direction * m_base_speed * m_ball.speed);
     if (m_ball.position.y() < 0 + m_ball.radius) {
         m_ball.position.y() = 0 + m_ball.radius;
         m_ball.direction.y() *= -1;
@@ -98,14 +151,48 @@ void PongPongApplication::moveBall()
         m_ball.position.y() = windowSize().y() - m_ball.radius;
         m_ball.direction.y() *= -1;
     }
-    if (m_ball.position.x() == 0 + m_ball.radius || m_ball.position.x() == windowSize().x() - m_ball.radius) {
-        m_ball.position = windowSize()/2;
+    if (m_ball.position.x() < 0 + m_ball.radius || m_ball.position.x() > windowSize().x() - m_ball.radius) {
+        if(m_ball.position.x() < 0 + m_ball.radius) {
+            ++m_right_score;
+            m_ball.direction = {-1,0};
+        }
+        else {
+            ++m_left_score;
+            m_ball.direction = {1,0};
+        }
+        m_ball.position = windowSize() / 2;
+        m_ball.speed = 1.;
     }
+    updateText();
     touchPaddle();
 }
 
-void PongPongApplication::touchPaddle(){
-    //TODO
+void PongPongApplication::touchPaddle()
+{
+    // left paddle interaction
+    if (m_ball.position.x() - m_ball.radius <= windowSize().x() / 80 + 5 + m_width_half &&
+        std::abs(m_ball.position.y() - m_y_left) < m_height_half) {
+        const auto relative_position = m_ball.position.y() - m_y_left;
+        const double position_ratio  = double(relative_position) / double(m_height_half);
+        m_ball.direction.x() *= -1.;
+        m_ball.direction.y() = position_ratio;
+        assert(m_ball.direction.length() > 0.);
+        m_ball.direction     = m_ball.direction.normalized();
+        m_ball.speed         = std::max((std::abs(position_ratio) + 0.5)*m_ball.speed, 1.);
+    }
+
+    // right paddle interaction
+    if (m_ball.position.x() + m_ball.radius >= windowSize().x() - windowSize().x() / 80 - 6 - m_width_half &&
+        std::abs(m_ball.position.y() - m_y_right) < m_height_half) {
+        const auto relative_position = m_ball.position.y() - m_y_right;
+        const double position_ratio  = double(relative_position) / double(m_height_half);
+        m_ball.direction.x() *= -1.;
+        m_ball.direction.y() = position_ratio;
+        assert(m_ball.direction.length() > 0.);
+        m_ball.direction     = m_ball.direction.normalized();
+        m_ball.speed         = std::max((std::abs(position_ratio) + 0.5)*m_ball.speed, 1.);
+    }
+    redraw();
 }
 
 void PongPongApplication::drawEvent()
@@ -116,53 +203,54 @@ void PongPongApplication::drawEvent()
     const auto size_y = windowSize().y();
     int window_size   = windowSize().product();
 
-    // Allocate storage for pixel colours
-    std::vector<Color3> colours(window_size);
 
-    //Give colours to image and get matrix
-    MutableImageView2D image_view(PixelFormat::RGB32F, windowSize(), {colours.data(), colours.size()});
-    auto pixel = image_view.pixels<Color3>();
-    for (auto x = 0; x < image_view.size().x(); x++) {
-        for (auto y = 0; y < image_view.size().y(); y++) {
-            pixel[y][x] = {0, 0.3, 0.8};
-        }
-    }
+    drawRectangle(m_image_view.size().x() / 80 + 5, m_y_left, m_image_view, {0, 0.3, 0.8});
+    drawRectangle(m_image_view.size().x() - m_image_view.size().x() / 80 - 6, m_y_right, m_image_view, {0, 0.3, 0.8});
 
     if (m_left_player_state == PlayerState::Up) {
-        m_y_left += size_y / 100;
+        m_y_left += m_paddle_speed;
         m_y_left = std::min(m_y_left, size_y - m_height_half - 1);
     }
     if (m_left_player_state == PlayerState::Down) {
-        m_y_left -= size_y / 100;
+        m_y_left -= m_paddle_speed;
         m_y_left = std::max(m_y_left, m_height_half);
     }
 
     if (m_right_player_state == PlayerState::Up) {
-        m_y_right += size_y / 100;
+        m_y_right += m_paddle_speed;
         m_y_right = std::min(m_y_right, size_y - m_height_half - 1);
     }
     if (m_right_player_state == PlayerState::Down) {
-        m_y_right -= size_y / 100;
+        m_y_right -= m_paddle_speed;
         m_y_right = std::max(m_y_right, m_height_half);
     }
 
     //Paint paddles and ball
-    drawRectangle(image_view.size().x() / 80 + 5, m_y_left, image_view, {1, 1, 1});
-    drawRectangle(image_view.size().x() - image_view.size().x() / 80 - 6, m_y_right, image_view, {1, 1, 1});
-    drawBall(image_view, {0.8, 0, 0.2});
-
+    drawRectangle(m_image_view.size().x() / 80 + 5, m_y_left, m_image_view, {1, 1, 1});
+    drawRectangle(m_image_view.size().x() - m_image_view.size().x() / 80 - 6, m_y_right, m_image_view, {1, 1, 1});
+    drawBall(m_image_view, {0, 0.3, 0.8});
     moveBall();
+    drawBall(m_image_view, {0.8, 0, 0.2});
 
     //upload pixels to GPU
     GL::Texture2D texture;
     texture.setWrapping(GL::SamplerWrapping::ClampToEdge)
         .setMaxAnisotropy(GL::Sampler::maxMaxAnisotropy())
-        .setStorage(1, GL::TextureFormat::RGB32F, image_view.size())
-        .setSubImage(0, {}, image_view);
+        .setStorage(1, GL::TextureFormat::RGB32F, m_image_view.size())
+        .setSubImage(0, {}, m_image_view);
 
     //draw texture using shader
     auto triangleShader = FullScreenTriangle{};
     triangleShader.bindTexture(texture).draw(GL::Mesh{}.setCount(3));
+
+    m_shader.bindVectorTexture(m_cache.texture());
+
+    m_shader
+        .setTransformationProjectionMatrix(m_transformationProjectionDynamicText)
+        .setColor(0xffffff_rgbf)
+        .setOutlineRange(0.5f, 1.0f)
+        .setSmoothness(0.075f)
+        .draw(m_dynamicText->mesh());
 
     swapBuffers();
     redraw();
--- PongPongApplication.h
+++ PongPongApplication.h
@@ -1,6 +1,10 @@
 #ifndef PONGPONG_PONGPONGAPPLICATION_H
 #define PONGPONG_PONGPONGAPPLICATION_H
 
+#include <Corrade/PluginManager/Manager.h>
+#include <Corrade/Utility/FormatStl.h>
+#include <Corrade/Utility/Resource.h>
+
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Math/Color.h>
@@ -10,7 +14,17 @@
 #include <Corrade/Containers/StridedArrayView.h>
 #include <Magnum/GL/Texture.h>
 #include <Magnum/GL/TextureFormat.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/Mesh.h>
+#include <Magnum/GL/Renderer.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Math/Complex.h>
+#include <Magnum/Math/Matrix3.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Shaders/DistanceFieldVectorGL.h>
+#include <Magnum/Text/AbstractFont.h>
+#include <Magnum/Text/DistanceFieldGlyphCache.h>
+#include <Magnum/Text/Renderer.h>
 
 #include <algorithm>
 #include <vector>
@@ -32,8 +46,12 @@ enum class PlayerState
 
 struct Ball{
     Vector2i position;
+
+    // unit length vector
     Vector2d direction;
     int radius;
+    // relative to a pixel base speed
+    double speed;
 };
 
 class PongPongApplication : public Platform::Application
@@ -49,6 +67,8 @@ private:
     void drawBall(const MutableImageView2D& image_view, const Color3& colour);
     void moveBall();
     void touchPaddle();
+    void updateText();
+
     int m_y_left;
     int m_y_right;
     int m_width_half;
@@ -56,6 +76,24 @@ private:
     Ball m_ball;
     PlayerState m_left_player_state = PlayerState::Idle;
     PlayerState m_right_player_state = PlayerState::Idle;
+
+    int m_paddle_speed;
+    double m_base_speed;
+
+    int m_left_score = 0, m_right_score = 0;
+
+    std::vector<Color3> m_colours;
+    MutableImageView2D m_image_view = MutableImageView2D(PixelFormat::RGB32F, {0,0}, nullptr);
+
+    PluginManager::Manager<Text::AbstractFont> m_manager;
+    Containers::Pointer<Text::AbstractFont> m_font;
+
+    Text::DistanceFieldGlyphCache m_cache;
+    GL::Buffer m_vertices, m_indices;
+    Containers::Pointer<Text::Renderer2D> m_dynamicText;
+    Shaders::DistanceFieldVectorGL2D m_shader;
+
+    Matrix3 m_projectionRotatingText, m_transformationProjectionDynamicText;
 };
 
 #endif //PONGPONG_PONGPONGAPPLICATION_H
